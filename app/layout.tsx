e86a2e8aec9e536d76fcdb7e511ce525c523823f
LudwigDassler <firsovivan2003@gmail.com>
2026-06-23 10:41:11 +0300

    remove next-auth session provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
-﻿"use client";
-import { SessionProvider } from "next-auth/react";
-import "./globals.css";
+﻿import "./globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ru">
+    <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       </head>
       <body style={{ margin: 0, padding: 0, overflowX: "hidden" }}>
-        <SessionProvider>{children}</SessionProvider>
+        {children}
       </body>
     </html>
   );

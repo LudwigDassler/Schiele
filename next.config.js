92f50e3f10fa0c588589837b2192f9e690345900
LudwigDassler <firsovivan2003@gmail.com>
2026-08-29 11:52:16 +0300

    Remove Tesseract from serverExternalPackages
    Removed Tesseract from serverExternalPackages in next.config.js.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // 🔥 ЗАЩИТА ВОРКЕРОВ: Говорим Webpack не трогать Tesseract при сборке
-  serverExternalPackages: ["tesseract.js"],
 };
-
 module.exports = nextConfig;
